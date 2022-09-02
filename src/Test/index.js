8e1597a71d4e50a3ac1ac45002a6b826b8204ee1
vijay kumar <vickyk25021997@gmail.com>
2022-09-02 13:39:27 +0530

    update package

--- src/Test/index.js
+++ src/Test/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
 //import './index.css';
-import styles from './Test.module.css';
-
+import styles from "./Test.module.css";
 
 const Test = () => {
-	return (
-		<div className={styles.test}>
-			<h1>test</h1>		
-		</div>
-	);
-}
+  return (
+    <div className={styles.test}>
+      <h1>test</h1>
+    </div>
+  );
+};
 
 export default Test;
